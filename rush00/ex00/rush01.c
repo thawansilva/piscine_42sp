58863ce8448bf251e4c72603f75b8ce4c972d70e
thawansilva <twnpsilva@gmail.com>
2025-07-13 13:32:32 -0300

    Addigin rush 00

---
+++ rush00/ex00/rush01.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thaperei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 17:58:02 by thaperei          #+#    #+#             */
+/*   Updated: 2025/04/20 20:14:07 by thaperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	print_char(int a, int b, int x, int y)
+{
+	int	is_top;
+	int	is_bottom;
+	int	is_left;
+	int	is_right;
+
+	is_top = (b == 0);
+	is_bottom = (b == y - 1);
+	is_left = (a == 0);
+	is_right = (a == x - 1);
+	if (is_top && is_left)
+		ft_putchar('/');
+	else if (is_top && is_right)
+		ft_putchar('\\');
+	else if (is_bottom && is_left)
+		ft_putchar('\\');
+	else if (is_bottom && is_right)
+		ft_putchar('/');
+	else if (is_top || is_bottom || is_left || is_right)
+		ft_putchar('*');
+	else
+		ft_putchar(' ');
+}
+
+void	rush(int x, int y)
+{
+	int	a;
+	int	b;
+
+	if (y < 0 || x < 0)
+		return ;
+	b = 0;
+	while (b < y)
+	{
+		a = 0;
+		while (a < x)
+		{
+			print_char(a, b, x, y);
+			a++;
+		}
+		write(1, "\n", 1);
+		b++;
+	}
+}
